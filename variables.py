d6583a3e6889445afd7d3a1df947a174f5a41bd2
sahilsharma2184 <sahilsharma2184@gmail.com>
2024-11-06 13:20:20 +0530

    Updated variables

--- variables.py
+++ variables.py
@@ -1,3 +1,10 @@
 #variales get created when you assign values to it
+#There is no command to declare a variable
 x=5
+y="Sahil"
 print(x)
+print(y)
+########
+a="Sahil" #variables need not to be declared with any type, their type can be changed after declaration, like below
+a="Sharma"
+print(a)
