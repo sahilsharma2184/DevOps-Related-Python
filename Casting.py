d19fe60609f5f96ca8c556002b565201cd6ff6c5
sahilsharma2184 <sahilsharma2184@gmail.com>
2024-11-06 17:55:09 +0530

    Casting and type() function file

--- Casting.py
+++ Casting.py
@@ -2,4 +2,11 @@
 x=str(5) #x is '5'
 y=int(3) #y is 
 z=float(6) #z is 6.0
-print(x,y,z) 
+print(x,y,z) #o/p: 5 3 6.0
+#############################
+
+#get the type of variable using type() function
+a="Sahil"
+b=9.8
+print(type(a))
+print(type(b))
