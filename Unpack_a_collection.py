b08b81629c7302f84c4be040e35044f98c0d57c4
sahilsharma2184 <sahilsharma2184@gmail.com>
2024-11-07 17:36:26 +0530

    Unpacking Collection

---
+++ Unpack_a_collection.py
@@ -0,0 +1,4 @@
+#a collection of values in a list, tuple etc. Python allows you to extract the values into variables. This is called unpacking
+collection=["Sahil","Sharma",10512]
+x,y,z=collection
+print(x,y,z)
