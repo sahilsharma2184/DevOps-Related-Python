d700929000a3796e0a7ef446eda5e91bcccd084c
sahilsharma2184 <sahilsharma2184@gmail.com>
2024-11-08 10:33:02 +0530

    Casting Data Type

--- Casting.py
+++ Casting.py
@@ -10,3 +10,22 @@ a="Sahil" #is same as 'Sahil' i.e. double quotes are same as single quotes, both
 b=9.8
 print(type(a))
 print(type(b))
+#############################
+
+p=int(1) #1
+q=int(9.7) #9
+r=int("3") #3
+print(p,q,r) # 1 9 3
+
+#############################
+s=float(1) #1.0
+t=float(8.6) #8.6
+u=float("3") #3.0
+v=float("4.6") #4.6
+print(s,t,u,v) #1.0 8.6 3.0 4.6
+
+#############################
+w=str("srmap") #'srmap'
+h=str("9.6") #'9.6'
+i=str("7") #'7'
+print(w,h,i) #srmap 9.6 7
