77ced7382decfa869b19fe369e6f9fb387b039b6
sahilsharma2184 <sahilsharma2184@gmail.com>
2024-11-08 13:10:39 +0530

    Concatenation

---
+++ Concatenate_Strings.py
@@ -0,0 +1,5 @@
+#String Concatenation: To combine two string use the '+' operator
+a="Sahil"
+b="Sharma"
+c=a+" "+b #or simply print(a+" "+b) returns the below output only
+print(c) #o/p: Sahil Sharma
